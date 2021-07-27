40b04adb61993fe11e0897e9ab40c3a0bb4c332e
vikeju <virginie.grolleau+github@protonmail.com>
2021-07-27 12:28:23 +0200

    Renom nav_home en nav simple | MAJ base.html.twig (nav) + security.yaml (path roles) + User.php (getRole) + MainController.php (lien quand on arrive sur site) + RegistrationController.php (setRole)

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,12 +4,20 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
     /**
-     * @Route("/", name="main_home")
+     * @Route("/", name="home_redirect")
+     */
+    public function home_redirect() {
+        return new RedirectResponse('/~wahac/sorties/public/login');
+    }
+
+    /**
+     * @Route("/main_home", name="main_home")
      */
     public function home() {
         return $this->render('main/home.html.twig');
